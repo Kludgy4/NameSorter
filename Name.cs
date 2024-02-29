7215058e41ecc26e7eb215cce2bf15bdbf51067d
Matthew Brian <matt@brians.com.au>
2024-02-29 12:40:05 +1100

    Add project files.

---
+++ Name.cs
@@ -0,0 +1,44 @@
+﻿namespace NameSorter;
+
+/**
+ * 
+ * A class that parses names from a given string
+ */
+class Name
+{
+    List<String> givenName;
+    List<String> lastNames;
+
+    /**
+     * Takes a space separated string name and creates a name object with it
+     * All space separated substrings, apart from the last one, are
+     * considered to be "given names". The last substring is the "last name".
+     * @param fullName - The string used to create the Name object
+     * 
+     */
+    public Name(string fullName)
+    {
+        string[] splitName = fullName.Split(" ", 4);
+        
+        if (splitName.Length < 2)
+        {
+            throw new FormatException("Not enough subnames provided");
+        }
+        
+        if (splitName[4].Contains(" "))
+        {
+            throw new FormatException("Too many subnames provided");
+        }
+
+        givenName = new List<string>();
+        givenName.Append(splitName[0]);
+
+        lastNames = new List<string>();
+        for (int i = 1; i < splitName.Length; ++i)
+        {
+            lastNames.Append(splitName[i]);
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace NameSorter;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Check that the right number of arguments have been passed in
+        if (args == null || args.Length != 1)
+        {
+            Console.WriteLine("usage: NameSorter inputNamesFile");
+            return;
+        }
+
+        // Check that the file name passed in is valid
+        FileStream fileStream = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+        StreamReader streamReader = new StreamReader(fileStream);
+        
+        // Read lines from the file until the end
+        List<String> lines = new List<String>();
+        while (!streamReader.EndOfStream )
+        {
+            string line = streamReader.ReadLine();
+            lines.Add(line);
+        }
+
+        Console.WriteLine(args[0]);
+        return;
+    }
+}
